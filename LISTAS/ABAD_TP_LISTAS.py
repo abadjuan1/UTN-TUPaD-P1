5695e75a67d30e76d462db0b4047c4d94e51d999
juan <abadjuan263@gmail.com>
2025-05-09 19:29:09 -0300

    Subiendo tp de listas

---
+++ LISTAS/ABAD_TP_LISTAS.py
@@ -0,0 +1,59 @@
+# 1) 
+
+#lista_multiplos_4 = list(range(4, 101, 4))
+#print(lista_multiplos_4)
+
+# 2)
+
+#lista_penultimo = [1, "Pedro", 4.5, True, "UTN"]
+#print(lista_penultimo[-2])
+
+# 3) 
+
+#lista_vacia = []
+#lista_vacia.append("Alumno")
+#lista_vacia.append("UTN")
+#lista_vacia.append("Profesor")
+#print(lista_vacia)
+
+# 4)
+#animales = ["perro", "gato", "conejo", "pez"]
+#animales[1] = "loro"
+#animales[-1] = "oso"
+#print(animales)
+
+# 5) El programa elimina el numero mayor de la lista numeros y luego la imprime en pantalla. 
+
+# 6)
+
+#lista_10_30 = list(range(10, 31, 5))
+#print(lista_10_30[0])
+#print(lista_10_30[1])
+
+# 7) 
+
+#autos = ["sedan", "polo", "suran", "gol"]
+#autos[1] = "kwid"
+#autos[2] = "onix"
+#print(autos)
+
+# 8) 
+
+#dobles = []
+#dobles.append(5*2)
+#dobles.append(10*2)
+#dobles.append(15*2)
+#print(dobles)
+
+# 9)
+
+#compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+#compras[2].append("jugo")
+#compras[1][1] = "tallarines"
+#compras[0].remove("pan")
+#print(compras)
+
+# 10) 
+
+#lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
+#print(lista_anidada)
